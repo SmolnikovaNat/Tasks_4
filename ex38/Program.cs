f3344afc471d8a32e9c9f390df6f497283491f59
Natalya <h.h.smolnikova@mail.ru>
2022-07-27 21:39:13 +0500

    задача 38

---
+++ ex38/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        //Задача 38. Задайте массив вещественных чисел.
+        //Найдите раздницу между мах и мин элементом массива
+        //[3,7,22,78] -> 76
+        
+        
+        public static Random random = new Random();
+
+        public static double[] RandomArrayGenerator()
+        {
+            double[] array = new double[random.Next(5, 11)];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(9, 10) + random.NextDouble();
+            }
+            return array;
+        }
+        
+        
+        public static void DistributionNumber(double[] array)
+        {
+            double min = array.First();
+            double max = array.First();
+            Console.Write(" ");
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (min > array[i]) min = array[i];
+
+                if (max > array[i]) max = array[i];
+                if (i != array.Length - 1) 
+                {
+                    Console.Write("{0}, ", Math.Round(array[i], 2));
+                }
+                else
+                {
+                     Console.Write("{0} -> {1}", Math.Round(array[i], 2), Math.Round(max - min));
+                }
+            }
+        }
+        static void Main(string[] asgs)
+        {
+           DistributionNumber(RandomArrayGenerator());
+        }
+    }
+}
